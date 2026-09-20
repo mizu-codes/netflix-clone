ff271942a1e3981e1fdf8c50a90ff31396242c2b
mizu-codes <mizhanmichu9966@gmail.com>
2026-09-20 21:19:11 +0530

    Add initial auth loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useAuth } from "./hooks/useAuth";
+import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
+  const { loading } = useAuth();
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
   return <AppRoutes />;
 }
 
